cda095fddc36fc213854d2ffb6f751e19b938f71
dlang5 <dlang5@student.rccd.edu>
2020-07-28 09:08:35 -0700

    Add files via upload

---
+++ Projects/Lang_Dennis_Project_1.1.1/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File: Connect Four Project
+ * Author: Dennis Lang
+ * Created on July 9, 2020, 11:13 PM
+ */
+
+#include "Player.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(int argc, char** argv) {
+    Player play;
+    Player arr[2]; //create 2 players, p1 and p2
+    char sixBy7[7][7]; //allow buffer for arr[1], '\n'?
+    cout << "Welcome to Connect Four!" << endl;
+    play.getSide(arr);
+    play.confirm(arr); 
+    play.gmeBord(sixBy7);
+    play.playGme(sixBy7, arr);
+    return 0;
+}
